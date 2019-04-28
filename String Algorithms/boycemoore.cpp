de4b2bd9600d7d33130934567c69052d3acf902f
Stanley Lin <stanleylin1@live.com>
2019-04-27 20:30:18 -0400

    add comments

--- String Algorithms/boycemoore.cpp
+++ String Algorithms/boycemoore.cpp
@@ -5,16 +5,16 @@ int boyceMoore(string search, string target){
     int table[256];
     int n = search.length();
     int k = target.length();
-    for(int i = 0; i < 256;i++){
+    for(int i = 0; i < 256;i++){ //create a table and set all values to the size of the target
         table[i] = k;
     }
-    for(int i = 0; i < k;i++){
+    for(int i = 0; i < k;i++){ //for each letter subtract distance till end of target
         table[target[i]] = k - 1 - i;
     }
     int i = 0, j = 0;
     while(i+k <= n){
         j = k - 1;
-        while(search[i+j] == target[j]){
+        while(search[i+j] == target[j]){ //if found compare the strings
             j-=1;
             if(j<0){
                 return i;
